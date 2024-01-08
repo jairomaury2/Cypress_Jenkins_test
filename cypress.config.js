5e49f472c5389c63ceb5bbc413605051f8a8d81e
jairomaury2 <156017205+jairomaury2@users.noreply.github.com>
2024-01-08 17:36:55 -0500

    Update cypress.config.js
    adjust mocha

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
+  //reporter: 'cypress-mochawesome-reporter',
+  /*reporterOptions: {
     "reportDir": "cypress/reports",
     "overwrite": true,
     "html": true,
@@ -17,12 +17,12 @@ module.exports = defineConfig({
     //overwrite: false,
     "experimentalMemoryManagement": true,
     "numTestsKeptInMemory": 1
-  },
+  }*/,
   
-  video: false,
-  e2e: {
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+  //video: false,
+  // e2e: {
+    //setupNodeEvents(on, config) {
+      //require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
   
